f74762e9c1a97ac049d973d89bf8aa439c697a3b
Israel Maytorena <i.velazquez@cignuz.com>
2024-12-29 23:22:37 -0600

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { i18n } from "$lib/i18n";
+	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+	import Header from './Header.svelte';
+	import '../app.css';
+	
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	<div class="app">
+<!--		<Header></Header>-->
+
+		<main>
+			{@render children()}
+		</main>
+
+		<footer>
+
+		</footer>
+	</div>
+</ParaglideJS>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
