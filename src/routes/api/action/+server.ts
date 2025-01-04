d4001bc0727c17477dc7eab31677e79fa3e3ef10
Israel Maytorena <i.velazquez@cignuz.com>
2025-01-03 19:19:07 -0600

    removing paraglide and lang services

--- src/routes/api/action/+server.ts
+++ src/routes/api/action/+server.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export async function POST({ request }) {
 	try {
 		const { id, count } = await request.json();
-		const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+		const data = { results: []}
 		const currentCount = id == 0 ? count - 1 : count + 1;
 
 		return json({ count: currentCount, data_mockup: data.results }, { status: 201 });
